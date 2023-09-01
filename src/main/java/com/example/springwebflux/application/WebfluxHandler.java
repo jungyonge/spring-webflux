082cb639ee43f1adde2497787f9b066268f9f943
jungyonge <jungyong_e@naver.com>
2023-09-01 17:15:06 +0900

    DB 연동

--- src/main/java/com/example/springwebflux/application/WebfluxHandler.java
+++ src/main/java/com/example/springwebflux/application/WebfluxHandler.java
@@ -1,11 +1,12 @@
 package com.example.springwebflux.application;
 
 import com.example.springwebflux.domain.WebfluxRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class WebfluxHandler {
 
     private final WebfluxRepository webfluxRepository;
